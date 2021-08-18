8046344ce0eadab8c0ebacee0f2cab47b4c2302d
Eduard  Jacobs <eduardjacobs@Eduards-MacBook-Pro.local>
2021-08-17 21:54:53 -0700

    remove extras

--- dist/lib/invoker/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvbGliL2ludm9rZXIvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbIiJdfQ==
